2e95df17fadfff2fa588b0684f80ab5253e07de9
HeynerAvila <heineravila@gmail.com>
2021-02-16 02:54:21 +0000

    Actualizado

--- src/main.js
+++ src/main.js
@@ -2,9 +2,34 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  pullNumber();
+  pullBothSuits();
 };
+
+let suits, numbers, suitsBottom, i;
+let card = {
+  suits: ["&#9830", "&#9829", "&#9824", "&#9827"],
+  suitsBottom: ["&#9830", "&#9829", "&#9824", "&#9827"],
+  numbers: ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+};
+
+function pullNumber() {
+  let theNumber = Math.floor(Math.random() * card.numbers.length);
+  document.getElementById("randomNumber").innerHTML = card.numbers[theNumber];
+}
+
+function pullBothSuits() {
+  let allSuits = Math.floor(Math.random() * card.suits.length);
+  document.getElementById("topSuit").innerHTML = card.suits[allSuits];
+  document.getElementById("bottomSuit").innerHTML = card.suits[allSuits];
+
+  if (card.suits[allSuits] === "&#9830" || card.suits[allSuits] === "&#9829") {
+    document.getElementById("theCard").classList.add("redSuit");
+  } else if (
+    card.suits[allSuits] === "&#9827" ||
+    card.suits[allSuits] === "&#9824"
+  ) {
+    document.getElementById("theCard").classList.remove("redSuit");
+  }
+}
